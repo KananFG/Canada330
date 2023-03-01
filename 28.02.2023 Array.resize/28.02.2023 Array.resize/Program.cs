ae9fe614cdba5f4999ee2c73fc2041b11d0b553b
KananFG <125909873+KananFG@users.noreply.github.com>
2023-03-01 17:07:05 +0400

    Array.resize metod

---
+++ 28.02.2023 Array.resize/28.02.2023 Array.resize/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._02._2023_Array.resize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numbers = { 1, 2, 3, 4 ,5};
+            //Array.Resize(ref numbers, numbers.Length + 1);
+
+            //foreach (var item in numbers)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Array.Resize(ref numbers, numbers.Length-1);
+
+            //foreach (int numb in numbers)
+            //{
+            //    Console.WriteLine(numb);
+            //}
+
+        }
+
+    }
+}

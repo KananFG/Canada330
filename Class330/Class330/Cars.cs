308e0f2489e55271dbf19be9de3b71f121c805ea
KananFG <125909873+KananFG@users.noreply.github.com>
2023-03-02 17:09:15 +0400

    Class created

---
+++ Class330/Class330/Cars.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class330
+{
+    class Cars
+    {
+        public string brand;
+        public string model;
+        public int year;
+
+        public void Scan()
+        {
+            Console.WriteLine($"{brand} {model} {year}");
+        }
+    }
+}
---
+++ Class330/Class330/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using Class330;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class330
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cars ca1 = new Cars()
+            {
+                brand = "Toyota",
+                model = "Camry",
+                year = 2021
+            };
+            Cars ca2 = new Cars()
+            {
+                brand = "Toyota",
+                model = "Rav4",
+                year = 2019
+            };
+            Cars ca3 = new Cars()
+            {
+                brand = "BMW",
+                model = "X7",
+                year = 2023
+            };
+            Cars ca4 = new Cars()
+            {
+                brand = "Mercedenz",
+                model = "GL",
+                year = 2008
+            };
+
+            ca1.Scan();
+            ca2.Scan();
+            ca3.Scan();
+            ca4.Scan();
+
+        }
+    }
+}
+
+ 
